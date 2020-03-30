885fa871bdf13e4eb329cbdd64e2fd41b583aa51
Oana Plesu <plesu.oana@gmail.com>
2020-03-30 22:34:13 +0300

    small fix

--- Tema1/src/com/company/DeliveryQueue.java
+++ Tema1/src/com/company/DeliveryQueue.java
@@ -5,49 +5,55 @@ public class DeliveryQueue {
 
     final Object queueIsEmpty = new Object();
     final Object queueIsFull  = new Object();
+    final Object addMutex     = new Object();
+    final Object getMutex     = new Object();
 
 
     public DeliveryQueue() {
     }
 
     public boolean addNewOrder(CakeOrder newOrder) {
-        synchronized (queueIsFull) {
-            setOrder(newOrder);
-            queueIsFull.notify();
-        }
-
-        try {
-            synchronized (queueIsEmpty) {
-                while (!isQueueEmpty()) {
-                    queueIsEmpty.wait();
-                }
+        synchronized (addMutex) {
+            synchronized (queueIsFull) {
+                setOrder(newOrder);
+                queueIsFull.notify();
             }
-            return true;
-        } catch (InterruptedException ignored) {
-        }
 
-        return false;
+            try {
+                synchronized (queueIsEmpty) {
+                    while (!isQueueEmpty()) {
+                        queueIsEmpty.wait();
+                    }
+                }
+                return true;
+            } catch (InterruptedException ignored) {
+            }
+
+            return false;
+        }
     }
 
     public CakeOrder getNextOrder() {
-        try {
-            synchronized (queueIsFull) {
-                while (!isQueueFull()) {
-                    queueIsFull.wait();
+        synchronized (getMutex) {
+            try {
+                synchronized (queueIsFull) {
+                    while (!isQueueFull()) {
+                        queueIsFull.wait();
+                    }
                 }
+            } catch (InterruptedException e) {
+                return null;
             }
-        } catch (InterruptedException e) {
-            return null;
+
+            CakeOrder currentOrder;
+
+            synchronized (queueIsEmpty) {
+                currentOrder = getOrder();
+                queueIsEmpty.notify();
+            }
+
+            return currentOrder;
         }
-
-        CakeOrder currentOrder;
-
-        synchronized (queueIsEmpty) {
-            currentOrder = getOrder();
-            queueIsEmpty.notify();
-        }
-
-        return currentOrder;
     }
 
     private synchronized boolean isQueueEmpty() {
